e2794644ef6f64cefe61dcbacad2035003a9acab
user_vsasaeva <vsasaeva@solvo.ru>
2018-03-01 17:41:33 +0300

    Create servlet

---
+++ L1.1_01/src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import templater.PageGenerator;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class AllRequestsServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", request.getParameter("key"));
+
+        response.getWriter().println(PageGenerator.instance().getPage("page.html", map));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
